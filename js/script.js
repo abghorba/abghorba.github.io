0a06cc08e79c2b993be624e9b661ee8d5bf76a7e
abghorba <andrewbghorbani@gmail.com>
2019-01-09 19:45:50 -0800

    Added feature for scrolling back to top

--- js/script.js
+++ js/script.js
@@ -4,6 +4,19 @@ $(function() {
     /* Enables parallax by creating a div with the .parallax-mirror class  */
     setParallaxImages();
 
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+            $('#return-to-top').fadeIn(200);    // Fade in the arrow
+        } else {
+            $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+        }
+    });
+    $('#return-to-top').click(function() {      // When arrow is clicked
+        $('body,html').animate({
+            scrollTop : 0                       // Scroll to top of body
+        }, 500);
+    });
+    
     /* Updates the URL with appropriate hash code on scrollspy event */
     $(window).on('activate.bs.scrollspy', function(e) {
         history.replaceState({}, "", $("a[href^='#']", e.target).attr("href"));
